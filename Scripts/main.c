d9cad13b1a79a6dad2d0a813b44d0d4dbce662dd
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-03-20 22:11:19 +0530

    Update main.c

--- Scripts/main.c
+++ Scripts/main.c
@@ -1,11 +1,12 @@
 /*AUTHOR Arkaaap
-IMPLEMENTING CAESAR CIPHER USING C 
+IMPLEMENTING CAESAR CIPHER USING C
 DATE 3/19/25
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define TRUE 1
 
 void Banner() {
     printf("  #####     #    #######  #####     #    ######      #####  ### ######  #     # ####### ######  \n");
@@ -17,13 +18,14 @@ void Banner() {
     printf("  #####  #     # #######  #####  #     # #     #     #####  ### #       #     # ####### #     # \n");
 }
 
-void encode (char *c,int key)
+void Caesar_Cipher (char *c,int key)
 {
+
     int l = strlen (c);
     char *c1 = (char *)malloc(l+1);
     for (int i=0;i<l;i++)
     {
-        if (c[i] >= 'A' && c[i] <= 'Z'){
+        if (c[i] >= 'A' && c[i] <= 'Z'|| c[i]>='a' && c[i]<='z' && c[i] == " "){
         c1[i] = c[i]+(key%26); // E = (x+key)%26
     }
     else {
@@ -35,16 +37,50 @@ void encode (char *c,int key)
 printf ("%s",c1);
 }
 
+void reverse_Cipher (char *c)
+{
+    int l = strlen(c);
+    for (int i=(l-1);i>=0;i--)
+    {
+        printf ("%c",c[i]);
+    }
+}
+
 int main (void)
 {
+    int n;
+    while (TRUE){
     Banner();
+    printf ("1.CAESAR_Cipher  :\n");
+    printf ("2.reverse _Cipher :\n");
+    printf ("3. EXIT\n");
+    printf ("Enter The Choice :\n");
+    scanf ("%d",&n);
     char c[100];
     int key;
-    printf ("Enter The String :");
-    scanf ("%s",c);
     printf ("\n");
-    printf ("Enter The Key :");
-    scanf ("%d",&key);
-    encode(c,key);
+
+    switch (n)
+    {
+        case 1:
+        printf ("Enter The String :");
+        scanf ("%s",c);
+        printf ("Enter The Key :");
+        scanf ("%d",&key);
+        Caesar_Cipher(c,key);
+        break;
+        case 2:
+        printf ("Enter The String :");
+        scanf ("%s",c);
+        reverse_Cipher(c);
+        break;
+        case 3:
+            exit (0x00);
+        default:
+            fprintf (stdout,"Invalid Choice !!");
+    }
+
+
     return (0x0);
+}
 }
